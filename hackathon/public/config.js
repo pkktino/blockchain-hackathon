2f64104682c1c69d84ee9816a83100bdf861dd44
NATTHANAN KUNCHOKWANIT (GISC-SI-TS-IT) <natthanan.k@cdg.co.th>
2018-09-08 14:47:26 +0700

    add date

--- hackathon/public/config.js
+++ hackathon/public/config.js
@@ -1,2 +1,2 @@
-const providerUrl = 'http://127.0.0.1:47777';
-const tokenAddr = '0xf6e23379387e37687b8698e75fe2054508925f26';
+const providerUrl = 'http://127.0.0.1:8546';
+const tokenAddr = '0x25336883b73fc7a4fa39cc93125efcb90f329896';
